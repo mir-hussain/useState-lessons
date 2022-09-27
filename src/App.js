bb4f358044073b4733afa09eebd4ef9e81387d34
mir-hussain <mirhussainmurtaza@gmail.com>
2022-09-27 23:15:01 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import LessonOne from "./pages/LessonOne";
+import LessonTwo from "./pages/LessonTwo";
+import LessonThree from "./pages/LessonThree";
+import LessonFour from "./pages/LessonFour";
+import Explain from "./pages/Explain";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen flex justify-center items-center bg-gray-100'>
+      {/* <LessonOne /> */}
+      {/* <LessonTwo /> */}
+      {/* <LessonThree /> */}
+      <LessonFour />
+      {/* <Explain /> */}
     </div>
   );
 }
